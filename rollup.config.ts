f86c242160b4a0c10b9df5f464a4a0d96d9f1419
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-11-24 19:59:21 +0900

    refactor: update Rollup configuration to handle circular dependencies and improve plugin setup
    chore: remove sourceMap option from tsconfig.json and clean up configuration

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,13 +2,28 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    esModule: true,
-    file: 'dist/index.js',
-    format: 'es',
-    sourcemap: true
-  },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
-}
+const config = [
+  {
+    input: 'src/index.ts',
+    output: {
+      esModule: true,
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: true
+    },
+    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
+    onwarn(warning, warn) {
+      if (
+        warning.code === 'CIRCULAR_DEPENDENCY' &&
+        warning.ids?.some((id) =>
+          /[/\\]node_modules[/\\]@actions[/\\]core/.test(id)
+        )
+      ) {
+        return
+      }
+      warn(warning)
+    }
+  }
+]
+
+export default config
